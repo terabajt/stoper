111c82bea0ed603e4a0198654129185e67797f62
Michał Pasynek <michalpasynek@gmail.com>
2022-08-02 12:51:37 +0200

    Add PAUSE button

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let minutes = 0
 let seconds = 0
 
 const handleStart = () => {
+	clearInterval(countTime)
 	countTime = setInterval(() => {
 		if (seconds < 9) {
 			seconds++
@@ -31,4 +32,8 @@ const handleStart = () => {
 	}, 1000)
 }
 
+const handlePause = () => {
+	clearInterval(countTime)
+}
 startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
