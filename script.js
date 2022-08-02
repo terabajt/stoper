ad8d7d699eb7add81dc1497253f6f9ef32e9ff29
Michał Pasynek <michalpasynek@gmail.com>
2022-08-02 12:27:29 +0200

    Add play function in JS code

---
+++ script.js
@@ -0,0 +1,34 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const historyBtn = document.querySelector('.start')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
+
+const infoBtn = document.querySelector('.info')
+const modalShadow = document.querySelector('.modal-shadow')
+const closeModalBtn = document.querySelector('.close')
+
+let countTime
+let minutes = 0
+let seconds = 0
+
+const handleStart = () => {
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++
+			stopwatch.textContent = `${minutes}:0${seconds}`
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++
+			stopwatch.textContent = `${minutes}:${seconds}`
+		} else {
+			minutes++
+			seconds = 0
+			stopwatch.textContent = `${minutes}:00`
+		}
+	}, 1000)
+}
+
+startBtn.addEventListener('click', handleStart)
